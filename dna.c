9a6aa5b6662f60285d6e3aa63d8646a64d9dd657
fernandocfbf <fernandocfbf@al.insper.edu.br>
2021-04-01 09:33:25 -0300

    passando nos testes de integraçao

--- dna.c
+++ dna.c
@@ -16,14 +16,51 @@ void load_string(FILE *file, int *p, char *s, int t) {
     fgets(s, *p + 1, file);
 }
 
-
 int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE + 1]) {
-    return 0;
+    
+    //base da recursão
+    if(n == 0 || m == 0){
+        length[n][m] = 0;
+    }
+
+    else if(length[n][m] != -1){
+        return length[n][m];
+    }
+
+    else if(a[n-1] == b[m-1]){
+        length[n][m] = mlcs_w(a, n-1, b, m-1, length) + 1;
+    }
+
+    else if(mlcs_w(a,n-1,b,m,length) > mlcs_w(a,n,b,m-1,length)){
+        length[n][m] = mlcs_w(a,n-1,b,m,length);
+    }
+
+    else{
+        length[n][m] = mlcs_w(a,n,b,m-1,length);
+    }
+
+    return length[n][m];
 }
 
-
 int mlcs(char a[], int n, char b[], int m) {
-    return 1;
+
+    int size = MAX_SIZE+1;
+
+    //inicializa a matriz
+    int length[size][size];
+
+    //percorre as linhas
+    for(int l=0; l<size; l++){
+
+        //percorre as colunas
+        for(int c=0; c<size; c++){
+            length[l][c] = -1;
+        }
+    }
+
+    int resp = mlcs_w(a, n, b, m, length);
+
+    return resp;
 }
 
 
